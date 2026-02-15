a6951c55a78ab0ade02c3d63813f4336b3bf6a92
George Giosmas <gegiosmas@gmail.com>
2026-02-15 14:47:41 +0200

    First Draft of next_turn()

--- main_game.py
+++ main_game.py
@@ -20,13 +20,28 @@ def print_info():
 def initial():
     global board, what_happened, history
 
-# Alternates the playing sequence between black and white
-def next_turn():
-    global what_happened
-
 # Examines if we have a Check, CheckMate, or Draw
 def examine():
     global board, what_happened, history
 
+# Alternates the playing sequence between black and white
+def next_turn():
+    global what_happened
+    
+    while True:
+        if what_happened == "Game Starts":
+            initial()
+        elif what_happened == "White Plays":
+            examine()
+            print(" ------------ Black Plays ------------ ")
+            Black_plays()
+        elif what_happened == "Black Plays":
+            examine()
+            print(" ------------ White Plays ------------ ")
+            White_plays()
+
 if __name__ == "__main__":
-    pass
+    board = Board()
+    what_happened = ""
+    history = []
+    
