7d743b445edf5a477913ef98adf5e22f16d56d42
George Giosmas <gegiosmas@gmail.com>
2026-03-04 14:24:28 +0200

    Finished initial board drawing

--- gui.py
+++ gui.py
@@ -1,13 +1,14 @@
 import tkinter as tk
 from tkinter import PhotoImage
+from main_game import *
 
 class ChessGameGUI():
     def __init__(self, root):
         #### Game Parameters
-
+        self.board = Board()
 
         #### GUI Parameters
-        self.board_width, self.board_height = 640, 640
+        self.board_width, self.board_height = 720, 720
         self.piece_width, self.piece_height = 80, 80
         root.title("Chess Game")
 
@@ -17,25 +18,13 @@ class ChessGameGUI():
         self.historyText = tk.StringVar()
         self.historyLabel = tk.Label(self.historyFrame, textvariable=self.historyText, font='Arial 20', width=40)
         self.historyLabel.pack(fill='x')
-
-        # Rank Frame
-        self.rankFrame = tk.Frame(root)
-        self.rankFrame.pack(side='left', fill='y')
-        self.rankLabel = tk.Label(self.rankFrame, text='1           2           3          4            5           6             7           8', bg='white')
-        self.rankLabel.pack(side='left', fill='y', expand=1)
         
         # Main/Canvas frame
         self.mainFrame = tk.Frame(root)
         self.mainFrame.pack()
-        self.canvas = tk.Canvas(self.mainFrame, width = self.board_width, height = self.board_height, bg='grey')
+        self.canvas = tk.Canvas(self.mainFrame, width = self.board_width, height = self.board_height, bg="#928777")
         self.canvas.pack(fill='both')
         self.draw_board()
-
-        # File Frame
-        self.fileFrame = tk.Frame(root)
-        self.fileFrame.pack(fill='x')
-        self.fileLabel = tk.Label(self.fileFrame, text='a          b           c           d           e           f           g           h', bg='white')
-        self.fileLabel.pack(fill='x', expand=1)
 
         # Button Frame
         self.buttonFrame = tk.Frame(root)
@@ -57,18 +46,22 @@ class ChessGameGUI():
         place = 0
         self.images = {}
         for x in "WB":
-            self.images[x] = [self.subimage(45*i, place, 45*(i+1), 45+place) for i in range(self.num_sprites)]
-            place += 45
+            self.images[x] = [self.subimage(80*i, place, 80*(i+1), 80+place) for i in range(self.num_sprites)]
+            place += 80
     
     def draw_board(self):
         colour = 'white'
         for rank in range(8):
-            if rank%2 == 0: colour = 'green'
-            else: colour = 'white'
+            if rank%2 == 0: colour = "#b58863"
+            else: colour = "#f0d9b5"
+            self.canvas.create_text(40, 80*rank + 40, text=self.board.from_index_get_rank(7-rank), font=("Times New Roman", 18), fill="#333333")
             for file in range(8):
-                self.canvas.create_rectangle(80*rank, 80*file, 80*(rank+1), 80*(file+1), fill=colour)
-                if colour == 'white': colour = 'green'
-                else: colour = 'white'
+                self.canvas.create_rectangle(80*(file+1), 80*rank, 80*(file+2), 80*(rank+1), fill=colour)
+                if colour == "#f0d9b5": colour = "#b58863"
+                else: colour = "#f0d9b5"
+        
+        for i in range(8):
+            self.canvas.create_text(80*(i+1)+40, 680, text=self.board.from_index_get_file(i), font=("Times New Roman", 18), fill="#333333")
 
     def game_restart(self):
         pass
