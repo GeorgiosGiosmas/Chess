c9fbc6319803cbe5526f9241b1c551283d54244a
George Giosmas <gegiosmas@gmail.com>
2026-03-06 15:10:00 +0200

    Added functions for conversions between gui and board coordinates and worked a little on make move

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import PhotoImage
 from board import *
 from piece import *
+import math
 
 class ChessGameGUI():
     def __init__(self, root, board: Board):
@@ -26,8 +27,7 @@ class ChessGameGUI():
         self.mainFrame.pack()
         self.canvas = tk.Canvas(self.mainFrame, width = self.board_width, height = self.board_height, bg="#928777")
         self.canvas.pack(fill='both')
-        self.canvas.bind("<Button-1>", self.select_piece)
-        self.canvas.bind("<B1-Motion>", self.move_piece)
+        self.canvas.bind("<Button-1>", self.move_piece)
         self.draw_board()
         self.draw_pieces()
 
@@ -77,14 +77,58 @@ class ChessGameGUI():
                 if piece != None:
                     self.canvas.create_image(80*(file+1), 80*rank, image=self.images[piece.__str__()], anchor='nw') 
 
-    def select_piece(self, event):
-        self.selected_piece = None
-        self.position_x, self.position_y = event.x, event.y
-        if( 80 <= self.position_x <= 720 and 0 <= self.position_y <= 640):
-            pass 
-
     def move_piece(self, event):
-        pass
+        self.highlight_colour = "#f7ec59"
+        self.position_x, self.position_y = event.x, event.y
+
+        if(80 <= self.position_x <= 720 and 0 <= self.position_y <= 640):
+            self.new_selected_square_x, self.new_selected_square_y  = math.floor(self.position_x/80.0), math.floor(self.position_y/80.0)
+            self.new_selected_square_x, self.new_selected_square_y = self.from_gui_to_board(self.new_selected_square_x, self.new_selected_square_y)
+
+            # If we don't have a selected square select it if has a piece on top of it
+            if(self.selected_square is None):
+                self.selected_square = self.board.board[self.new_selected_square_y][self.new_selected_square_x]
+                piece = self.selected_square.piece_on_square
+                if(piece is not None):
+                    self.redraw_square(self.selected_square, self.new_selected_square_x, self.new_selected_square_y, self.highlight_colour)
+                    self.old_selected_square_x, self.old_selected_square_y = self.new_selected_square_x, self.new_selected_square_y
+
+            # Pick a new square or decide the move for the selected square
+            else:
+                # If we click on another square with our piece highlight it and reverse the highlighted square to its original colour
+
+
+
+            # Check if it already highlighted. If it is reverse it to its original colour
+            if(self.highlight_colour == self.squares_colour):
+                self.new_selected_square_x, self.new_selected_square_y = self.from_board_to_gui(self.new_selected_square_x, self.new_selected_square_y)
+                self.redraw_square(self.selected_square, self.new_selected_square_x, self.new_selected_square_y, self.squares_original_colour)
+                
+            # If square it is not highlighed, highlight it
+            else:
+                self.new_selected_square_x, self.new_selected_square_y = self.from_board_to_gui(self.new_selected_square_x, self.new_selected_square_y)
+                self.redraw_square(self.selected_square, self.new_selected_square_x, self.new_selected_square_y, self.highlight_colour)
+                self.squares_original_colour = self.squares_colour
+            
+            # If the square is not the highlighted square, reverse the highlighed square to its original colour and highlight the new one
+
+    def redraw_square(self, square, x, y, colour):
+        self.canvas.create_rectangle(80*x, 80*y, 80*(x+1), 80*(y+1), fill=colour)
+        piece = square.piece_on_square
+        if(piece is not None):
+            self.canvas.create_image(80*x, 80*y, image=self.images[piece.__str__()], anchor='nw') 
+
+    def from_gui_to_board(self, x, y):
+        return x - 1, 7 - y
+    
+    def from_board_to_gui(self, x, y):
+        return x + 1, 7 - y
+    
+    def get_square_colour(self, rank, file):
+        if (rank + file) % 2 == 0:
+            return "#b58863"
+        else:
+            return "#f0d9b5"
 
     def start_game(self):
         pass
